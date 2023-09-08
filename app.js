579095ae73725a2ae725f61c3c72419c4f5df032
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-09-08 22:21:18 +0300

    added user selection code logic

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-// const start = function () {
-//   console.log('Game is starting...');
-// };
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
 
-// console.dir(startGame);
+let gameIsRunning = false;
+
+const getPlayerChoice = function () {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ''
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
 
 startGameBtn.addEventListener('click', function () {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
   console.log('Game is starting...');
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
 });
