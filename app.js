ae722dceb1d9f7f0e5483d137cd7398bf66a43de
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-09-08 22:00:06 +0300

    added anonymous function to startGameBtn event listener

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-const start = function () {
-  console.log('Game is starting...');
-};
+// const start = function () {
+//   console.log('Game is starting...');
+// };
 
 // console.dir(startGame);
 
-startGameBtn.addEventListener('click', start);
+startGameBtn.addEventListener('click', function () {
+  console.log('Game is starting...');
+});
