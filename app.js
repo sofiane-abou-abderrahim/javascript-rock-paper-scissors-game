35c1defd30b8b20e5270c52cb32de14d69930cab
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-09-09 11:33:29 +0300

    added code logic in startGameBtn event listener to output messages to the user

--- app.js
+++ app.js
@@ -63,5 +63,14 @@ startGameBtn.addEventListener('click', () => {
   const playerChoice = getPlayerChoice();
   const computerChoice = getComputerChoice();
   const winner = getWinner(computerChoice, playerChoice);
-  console.log(winner);
+  let message = `You picked ${playerChoice}, computer picked ${computerChoice}, therefore you `;
+  if (winner === RESULT_DRAW) {
+    message = message + 'had a draw.';
+  } else if (winner === RESULT_PLAYER_WINS) {
+    message = message + 'won.';
+  } else {
+    message = message + 'lost.';
+  }
+  alert(message);
+  gameIsRunning = false;
 });
