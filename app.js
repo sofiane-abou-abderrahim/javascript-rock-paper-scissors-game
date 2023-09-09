5b14d2c697bef25faafb98067baea5be547dc12a
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-09-09 22:52:48 +0300

    tweaked sumUp & substractUp functions + used bind() on combine function

--- app.js
+++ app.js
@@ -84,31 +84,61 @@ startGameBtn.addEventListener('click', () => {
 
 // not related to the game
 
-const sumUp = (resultHandler, ...numbers) => {
+const combine = (resultHandler, operation, ...numbers) => {
   const validateNumber = number => {
     return isNaN(number) ? 0 : number;
   };
 
   let sum = 0;
   for (const num of numbers) {
-    sum += validateNumber(num);
+    if (operation === 'ADD') {
+      sum += validateNumber(num);
+    } else {
+      sum -= validateNumber(num);
+    }
   }
   resultHandler(sum);
 };
 
-const subtractUp = function () {
-  let sum = 0;
-  for (const num of arguments) {
-    // don't use that ("arguments" is not recommanded)
-    sum -= num;
-  }
-  return sum;
+// const subtractUp = function (resultHandler, ...numbers) {
+//   let sum = 0;
+//   for (const num of numbers) {
+//     sum -= num;
+//   }
+//   resultHandler(sum);
+// };
+
+const showResult = (messageText, result) => {
+  alert(messageText + ' ' + result);
 };
 
-const showResult = result => {
-  alert('The result after adding all numbers is: ' + result);
-};
-
-sumUp(showResult, 1, 5, 'hi', -3, 6, 10);
-sumUp(showResult, 1, 5, 10, -3, 6, 10, 25, 88);
-console.log(subtractUp(1, 10, 15, 20));
+combine(
+  showResult.bind(this, 'The result after adding all numbers is: '),
+  'ADD',
+  1,
+  5,
+  'hi',
+  -3,
+  6,
+  10
+);
+combine(
+  showResult.bind(this, 'The result after adding all numbers is: '),
+  'ADD',
+  1,
+  5,
+  10,
+  -3,
+  6,
+  10,
+  25,
+  88
+);
+combine(
+  showResult.bind(this, 'The result after subtracting all numbers is: '),
+  'SUBTRACT',
+  1,
+  10,
+  15,
+  20
+);
