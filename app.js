2faa0e768b4d896091120e4e096721cfccecbc1b
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-09-09 12:52:20 +0300

    showed one function with rest parameter and another one with arguments key word

--- app.js
+++ app.js
@@ -81,3 +81,26 @@ startGameBtn.addEventListener('click', () => {
   alert(message);
   gameIsRunning = false;
 });
+
+// not related to the game
+
+const sumUp = (...numbers) => {
+  let sum = 0;
+  for (const num of numbers) {
+    sum += num;
+  }
+  return sum;
+};
+
+const subtractUp = function () {
+  let sum = 0;
+  for (const num of arguments) {
+    // don't use that ("arguments" is not recommanded)
+    sum -= num;
+  }
+  return sum;
+};
+
+console.log(sumUp(1, 5, 10, -3, 6, 10));
+console.log(sumUp(1, 5, 10, -3, 6, 10, 25, 88));
+console.log(subtractUp(1, 10, 15, 20));
